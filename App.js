a67795058bd8564a16301f0f3b5b22a4d91ff71c
Benjamin Konopka <benjamin.konopka@icloud.com>
2021-05-27 17:06:18 +0200

    feat: 🎸 add firebase and account screens

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
 import { ThemeProvider } from "styled-components/native";
+
+import * as firebase from "firebase";
+
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
@@ -13,6 +17,20 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAf5lj0ZaNUWOSmr0fJQLPcxZRmjCEL1kw",
+  authDomain: "mealstogo-8e132.firebaseapp.com",
+  projectId: "mealstogo-8e132",
+  storageBucket: "mealstogo-8e132.appspot.com",
+  messagingSenderId: "57199793393",
+  appId: "1:57199793393:web:176ad82294f712e47d4550",
+  measurementId: "G-W5NH2P9FGF",
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -25,16 +43,19 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
